bcdcb56c99757d43ea7e42122d38ca60eba90e63
Chantel Anderson <shanybug98@gmail.com>
2025-04-19 16:44:44 -0700

    Update problem3.py

--- Mod2/problem3.py
+++ Mod2/problem3.py
@@ -1,1 +1,20 @@
+'''# 3. Create a program that prompts for the side lengths of 
+a triangle and computes the area using Heron's formula.'''
 
+import math
+
+# prompt the user for three side lengths
+a = int(input("enter side length a:"))
+b = int(input("enter side length b:"))
+c = int(input("enter side length c:"))
+
+# Compute the semi-perimeter
+s = (a + b + c) / 2
+
+# Compute the value inside sqrt
+value_inside_sqrt = s * (s - a) * (s - b) * (s - c)
+
+# Compute the area only if the value inside sqrt is non-negative
+area = math.sqrt(abs(value_inside_sqrt)) 
+
+print(f"the area of the triangle is: {area:.2f}")
