d875270ee8f1c18029b5a9ae8e2497e78d4fc194
Chantel Anderson <shanybug98@gmail.com>
2025-04-19 14:56:14 -0700

    Update problem2.py

--- Mod2/problem2.py
+++ Mod2/problem2.py
@@ -1,1 +1,10 @@
 
+'''# 2. Create a program
+that prompts for 2 numbers and then outputs the
+addition, subtraction, multiplication, and division of
+the first number by the second number.'''
+
+print(10+5)
+print(20-9)
+print(30*8)
+print(15/5)
