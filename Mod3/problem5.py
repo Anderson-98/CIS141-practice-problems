8703b504bec2253cee76a1b8631397d935ce6df3
Chantel Anderson <shanybug98@gmail.com>
2025-04-27 18:16:44 -0700

    Update problem5.py

--- Mod3/problem5.py
+++ Mod3/problem5.py
@@ -1,2 +1,5 @@
 #5. Print 3 words with a | character (called a pipe) in between them.
 Use the appropriate keyword argument in print() to do so.
+
+# Print three words separated by a pipe character
+print("flour", "suagr", "eggs", sep="|")
