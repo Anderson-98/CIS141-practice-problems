ca44b443818c0590c0c308b681eff3afa7614553
Chantel Anderson <shanybug98@gmail.com>
2025-04-27 18:07:02 -0700

    Update probmle3.py

--- Mod3/probmle3.py
+++ Mod3/probmle3.py
@@ -1,2 +1,10 @@
 #3. Prompt the user for a sentence and a word to try to find in that sentence.
 Have the program print out whether the word was found in the sentence. (i.e. True or False)
+# Prompt the user for a sentence
+sentence = input("Enter a sentence: ")
+
+# Prompt the user for a word to search for in the sentence
+word = input("Enter a word to find in the sentence: ")
+
+# Check if the word is in the sentence and print the result
+print(word in sentence)
