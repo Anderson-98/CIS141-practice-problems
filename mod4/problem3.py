041e0f6042aaf940977ab62b6eba6f8b1b2a0002
Chantel Anderson <shanybug98@gmail.com>
2025-05-04 22:59:48 -0700

    Create problem3.py

---
+++ mod4/problem3.py
@@ -0,0 +1,10 @@
+# Write your code here :-)
+#3. Prompt the user for their bank balance. 
+# Evaluate whether the balance is less than $100. 
+# Print True if the user’s balance is below $100; print False, otherwise.
+
+#get the users bank balance 
+balance = int(input("Pleas state your bank balance: "))
+
+# determin if the users balance is less the $100 print True if yes print False if no 
+print(balance < 100)
