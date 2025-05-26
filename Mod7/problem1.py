41b61c6acfbaa144985dac8291f9237324f09d6f
Chantel Anderson <shanybug1998@gmail.com>
2025-05-25 22:02:02 -0700

    Update problem1.py

--- Mod7/problem1.py
+++ Mod7/problem1.py
@@ -1,1 +1,19 @@
+#1. Write a function called count_vowels(input) that takes a string
+# and returns the number of vowels (a, e, i, o, u) in it.
+
+
+
+def count_vowels(input):
+    vowels = "aeiou"
+    count = 0  
+    
+    for char in input.lower():  
+        if char in vowels:  
+            count += 1  
+    
+    return count 
+
+
+print(count_vowels("Hello World"))  
+print(count_vowels("Python"))       
 
